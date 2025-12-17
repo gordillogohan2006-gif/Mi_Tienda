7abf2d3220e4ce26d2d925077ac2c06d0ca29c75
gordillogohan2006-gif <gordillogohan2006@gmail.com>
2025-12-17 12:08:24 -0500

    Refactor database connection constants in conexion.php

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,12 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "tiendadb";
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', ''); 
+define('DB_NAME', 'tiendadb');
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión crítica: " . $conn->connect_error);
 }
+
 $conn->set_charset("utf8mb4");
 ?>
